669b740b0a09115d0d5b7d64b58162f520b8fe4c
phomfirst <70604410+phomfirst@users.noreply.github.com>
2020-09-15 14:44:00 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 // JavaScript Document
+
+var navBtn = document.querySelector('.menu-btn');
+
+function navBar() {
+    var x = document.querySelector('.nav-menu');
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+navBtn.addEventListener('click', navBar);
